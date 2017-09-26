75e83948f09a9eb323d9384dd162e9580dfc1cdc
takshil.bharadwaj <takshil.bharadwaj@CSELAB02-05.bennett.edu.in>
2017-09-26 11:36:47 +0530

    All of the required files.

---
+++ src/JUnitTestExamplePackage/JUnitTesting.java
@@ -0,0 +1,10 @@
+package JUnitTestExamplePackage;
+
+public class JUnitTesting {
+	public int addNumbers(int a, int b){
+		return(a+b);
+	}
+	public String addStrings(String a, String b){
+		return(a+b);
+	}
+}
---
+++ src/JUnitTestExamplePackage/TestAddNumbers.java
@@ -0,0 +1,15 @@
+package JUnitTestExamplePackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAddNumbers {
+
+	@Test
+	public void test() {
+		JUnitTesting junit = new JUnitTesting();
+		int result = junit.addNumbers(250,750);
+		assertEquals(1000,result);
+	}
+	}
